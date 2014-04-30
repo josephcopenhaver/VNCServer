905dd585b057fa23601972050b62440610590ec9
Joseph Copenhaver <joseph.copenhaver@gmail.com>
2014-04-30 12:50:51 -0500

    CurrentProcessInfo is now readable and less round about

--- src/com/jcope/util/CurrentProcessInfo.java
+++ src/com/jcope/util/CurrentProcessInfo.java
@@ -13,15 +13,14 @@ public class CurrentProcessInfo
     
 	public static Long getPID() throws IOException
 	{
-	    Long[] untaintedSrcLong = new Long[]{null};
-		String stringRep = getPIDStr(untaintedSrcLong);
+	    Long rval = null;
+		String stringRep = getPIDStr();
 		
 		if (stringRep != null)
 		{
-		    untaintedSrcLong[0] = null;
 		    try
 		    {
-		        untaintedSrcLong[0] = Long.parseLong(stringRep);
+		        rval = Long.parseLong(stringRep);
 		    }
 		    catch (NumberFormatException e)
 		    {
@@ -29,77 +28,63 @@ public class CurrentProcessInfo
 		    }
 		}
 		
-		return untaintedSrcLong[0];
+		return rval;
+	}
+	
+	private static boolean isNumRPlus(String str)
+	{
+		boolean rval = str.matches("[1-9]\\d+");
+		
+		return rval;
 	}
 	
 	public static String getPIDStr() throws IOException
 	{
-	    return getPIDStr(null);
-	}
-	
-	private static String getPIDStr(Long[] untaintedSrcLong) throws IOException
-	{
 		String rval;
+		
 		try
 		{
 			rval = new File("/proc/self").getCanonicalFile().getName();
-			if (!rval.matches("[1-9]\\d+"))
+			if (!isNumRPlus(rval))
 			{
 				throw new IOException("Could not identify PID");
 			}
 		}
 		catch (IOException e)
 		{
-			Object pid = getWindowsPID(untaintedSrcLong == null ? String.class : Long.class);
-			if (pid == null)
-			{
-				throw(e);
-			}
-			else if (untaintedSrcLong == null)
-		    {
-		        rval = (String) pid;
-		    }
-		    else
-		    {
-		        untaintedSrcLong[0] = (Long) pid;
-                rval = null;
-		    }
+			rval = getWindowsPID();
 		}
+		
 		return rval;
 	}
 	
-	private static Object getWindowsPID(Class<?> wantedReturnType)
+	private static String getWindowsPID()
 	{
+		String rval = null;
+		String tmpStr;
+
 	    // something like '<pid>@<hostname>', at least in SUN / Oracle JVMs
 	    final String jvmName = ManagementFactory.getRuntimeMXBean().getName();
 	    final int index = jvmName.indexOf('@');
 
-	    if (index < 1)
-	    {
-	        // part before '@' empty (index = 0) / '@' not found (index = -1)
-	        return null;
-	    }
+		do
+		{
+			if (index < 1)
+			{
+				// part before '@' empty (index = 0) / '@' not found (index = -1)
+				break;
+			}
 
-	    try
-	    {
-	        long tmp = Long.parseLong(jvmName.substring(0, index));
-	        if (wantedReturnType == Long.class)
-	        {
-	            return tmp;
-	        }
-	        else if (wantedReturnType == String.class)
-	        {
-	            return Long.toString(tmp);
-	        }
-	        else
-	        {
-	            assert_(false);
-	        }
-	    }
-	    catch (NumberFormatException e)
-	    {
-	        // ignore
-	    }
-	    return null;
+			tmpStr = jvmName.substring(0, index);
+			
+			if (!isNumRPlus(tmpStr))
+			{
+				break;
+			}
+			
+			rval = tmpStr;
+		} while (Boolean.FALSE);
+		
+	    return rval;
 	}
 }
