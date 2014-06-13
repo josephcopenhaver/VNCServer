1e2a35a931fd5ec0e0aeb88e19084be74fed3c82
Joseph Copenhaver <joseph.copenhaver@gmail.com>
2014-06-13 02:11:00 -0500

    Modal builds were not always including event handler classes because there were no code references to the classes via import statements. Bootstrappers have been added to Generic event Handler subclasses in lieu of using a recursive package inspection utility (google guava and reflections)

--- src/com/jcope/vnc/client/input/Handler.java
+++ src/com/jcope/vnc/client/input/Handler.java
@@ -2,11 +2,35 @@ package com.jcope.vnc.client.input;
 
 import static com.jcope.debug.Debug.assert_;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.Semaphore;
 
 import com.jcope.debug.LLog;
 import com.jcope.vnc.client.StateMachine;
+import com.jcope.vnc.client.input.handle.AliasChanged;
+import com.jcope.vnc.client.input.handle.AliasDisconnected;
+import com.jcope.vnc.client.input.handle.AliasRegistered;
+import com.jcope.vnc.client.input.handle.AliasUnregistered;
+import com.jcope.vnc.client.input.handle.AuthorizationUpdate;
+import com.jcope.vnc.client.input.handle.ChatMsgToAll;
+import com.jcope.vnc.client.input.handle.ChatMsgToUser;
+import com.jcope.vnc.client.input.handle.ClientAliasUpdate;
+import com.jcope.vnc.client.input.handle.ClipboardChanged;
+import com.jcope.vnc.client.input.handle.ConnectionClosed;
+import com.jcope.vnc.client.input.handle.ConnectionEstablished;
+import com.jcope.vnc.client.input.handle.CursorGone;
+import com.jcope.vnc.client.input.handle.CursorMove;
+import com.jcope.vnc.client.input.handle.FailedAuthorization;
+import com.jcope.vnc.client.input.handle.GetClipboard;
+import com.jcope.vnc.client.input.handle.NumScreensChanged;
+import com.jcope.vnc.client.input.handle.ReadInputEvents;
+import com.jcope.vnc.client.input.handle.ScreenGone;
+import com.jcope.vnc.client.input.handle.ScreenResized;
+import com.jcope.vnc.client.input.handle.ScreenSegmentChanged;
+import com.jcope.vnc.client.input.handle.ScreenSegmentSizeUpdate;
+import com.jcope.vnc.client.input.handle.ScreenSegmentUpdate;
+import com.jcope.vnc.client.input.handle.SetClipboard;
 import com.jcope.vnc.shared.StateMachine.CLIENT_EVENT;
 import com.jcope.vnc.shared.StateMachine.SERVER_EVENT;
 import com.jcope.vnc.shared.input.Handle;
@@ -19,8 +43,53 @@ public class Handler extends com.jcope.vnc.shared.input.Handler<StateMachine, SE
     private static final Semaphore instanceSema = new Semaphore(1, true);
     private static final Handler[] selfRef = new Handler[]{null};
     
+    // enumerating linkage here so top down compilations will include these classes
+    private static Class<?>[] bootStraps = new Class<?>[]{
+        AliasChanged.class,
+        AliasDisconnected.class,
+        AliasRegistered.class,
+        AliasUnregistered.class,
+        AuthorizationUpdate.class,
+        ChatMsgToAll.class,
+        ChatMsgToUser.class,
+        ClientAliasUpdate.class,
+        ClipboardChanged.class,
+        ConnectionClosed.class,
+        ConnectionEstablished.class,
+        CursorGone.class,
+        CursorMove.class,
+        FailedAuthorization.class,
+        GetClipboard.class,
+        NumScreensChanged.class,
+        ReadInputEvents.class,
+        ScreenGone.class,
+        ScreenResized.class,
+        ScreenSegmentChanged.class,
+        ScreenSegmentSizeUpdate.class,
+        ScreenSegmentUpdate.class,
+        SetClipboard.class
+    };
+    
     static
     {
+        ArrayList<Class<?>> bootStrapper;
+        if (bootStraps != null)
+        {
+            try {
+                bootStrapper = new ArrayList<Class<?>>(bootStraps.length);
+                for (Class<?> clazz : bootStraps)
+                {
+                    bootStrapper.add(clazz);
+                }
+            }
+            finally {
+                bootStraps = null;
+            }
+        }
+        else
+        {
+            bootStrapper = null;
+        }
         String thisClassName = Handler.class.getName();
         int basenameSep = thisClassName.lastIndexOf('.');
         String thisClassDir = thisClassName.substring(0, basenameSep);
@@ -33,6 +102,17 @@ public class Handler extends com.jcope.vnc.shared.input.Handler<StateMachine, SE
                 String eventName = formatNameCamelCase(eventCodeName);
                 String className = String.format("%s.handle.%s", thisClassDir, eventName);
                 Class<?> c = Class.forName(className);
+                if (bootStrapper != null)
+                {    
+                    if (bootStrapper.contains(c))
+                    {
+                        bootStrapper.remove(c);
+                    }
+                    else
+                    {
+                        LLog.e(new ClassNotFoundException(String.format("Class %s is not in the bootStraps set in %s", c.getSimpleName(), Handler.class.getName())), true, true);
+                    }
+                }
                 Object handle = c.newInstance();
                 assert_(handle instanceof Handle);
                 assert_(((Handle<?>)handle).getType() == StateMachine.class);
@@ -58,6 +138,10 @@ public class Handler extends com.jcope.vnc.shared.input.Handler<StateMachine, SE
             {
                 LLog.e(e);
             }
+        }
+        if (null != bootStrapper && !bootStrapper.isEmpty())
+        {
+            LLog.e(new ClassNotFoundException(String.format("Class %s in the bootStraps set was never instantiated in %s", bootStrapper.get(0).getSimpleName(), Handler.class.getName())), true, true);
         }
     }
     
--- src/com/jcope/vnc/server/input/Handler.java
+++ src/com/jcope/vnc/server/input/Handler.java
@@ -2,11 +2,23 @@ package com.jcope.vnc.server.input;
 
 import static com.jcope.debug.Debug.assert_;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.Semaphore;
 
 import com.jcope.debug.LLog;
 import com.jcope.vnc.server.ClientHandler;
+import com.jcope.vnc.server.input.handle.AcknowledgeNonSerialEvent;
+import com.jcope.vnc.server.input.handle.ClipboardChanged;
+import com.jcope.vnc.server.input.handle.EnableAliasMonitor;
+import com.jcope.vnc.server.input.handle.EnableConnectionMonitor;
+import com.jcope.vnc.server.input.handle.GetClipboard;
+import com.jcope.vnc.server.input.handle.GetScreenSegment;
+import com.jcope.vnc.server.input.handle.OfferInput;
+import com.jcope.vnc.server.input.handle.RequestAlias;
+import com.jcope.vnc.server.input.handle.SelectScreen;
+import com.jcope.vnc.server.input.handle.SendChatMsg;
+import com.jcope.vnc.server.input.handle.SetClipboard;
 import com.jcope.vnc.shared.StateMachine.CLIENT_EVENT;
 import com.jcope.vnc.shared.input.Handle;
 
@@ -18,8 +30,41 @@ public class Handler extends com.jcope.vnc.shared.input.Handler<ClientHandler, C
     private static final Semaphore instanceSema = new Semaphore(1, true);
     private static final Handler[] selfRef = new Handler[]{null};
     
+    // enumerating linkage here so top down compilations will include these classes
+    private static Class<?>[] bootStraps = new Class<?>[]{
+        AcknowledgeNonSerialEvent.class,
+        ClipboardChanged.class,
+        EnableAliasMonitor.class,
+        EnableConnectionMonitor.class,
+        GetClipboard.class,
+        GetScreenSegment.class,
+        OfferInput.class,
+        RequestAlias.class,
+        SelectScreen.class,
+        SendChatMsg.class,
+        SetClipboard.class
+    };
+    
     static
     {
+        ArrayList<Class<?>> bootStrapper;
+        if (bootStraps != null)
+        {
+            try {
+                bootStrapper = new ArrayList<Class<?>>(bootStraps.length);
+                for (Class<?> clazz : bootStraps)
+                {
+                    bootStrapper.add(clazz);
+                }
+            }
+            finally {
+                bootStraps = null;
+            }
+        }
+        else
+        {
+            bootStrapper = null;
+        }
         String thisClassName = Handler.class.getName();
         int basenameSep = thisClassName.lastIndexOf('.');
         String thisClassDir = thisClassName.substring(0, basenameSep);
@@ -32,6 +77,17 @@ public class Handler extends com.jcope.vnc.shared.input.Handler<ClientHandler, C
                 String eventName = formatNameCamelCase(eventCodeName);
                 String className = String.format("%s.handle.%s", thisClassDir, eventName);
                 Class<?> c = Class.forName(className);
+                if (bootStrapper != null)
+                {    
+                    if (bootStrapper.contains(c))
+                    {
+                        bootStrapper.remove(c);
+                    }
+                    else
+                    {
+                        LLog.e(new ClassNotFoundException(String.format("Class %s is not in the bootStraps set in %s", c.getSimpleName(), Handler.class.getName())), true, true);
+                    }
+                }
                 Object handle = c.newInstance();
                 assert_(handle instanceof Handle);
                 assert_(((Handle<?>)handle).getType() == ClientHandler.class);
@@ -57,6 +113,10 @@ public class Handler extends com.jcope.vnc.shared.input.Handler<ClientHandler, C
             {
                 LLog.e(e);
             }
+        }
+        if (null != bootStrapper && !bootStrapper.isEmpty())
+        {
+            LLog.e(new ClassNotFoundException(String.format("Class %s in the bootStraps set was never instantiated in %s", bootStrapper.get(0).getSimpleName(), Handler.class.getName())), true, true);
         }
     }
     
